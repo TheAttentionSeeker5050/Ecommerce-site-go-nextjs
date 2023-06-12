b985a373fa0e7912b794181aa6c1d3ca6a99a93d
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-06-12 11:50:19 -0300

    before setting typescript

--- ecommerce-go-server/main.go
+++ ecommerce-go-server/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// define the type of environment
+	environmentString := os.Getenv("ENVIRONMENT")
+	// check for errors
+	if environmentString == "" {
+		environmentString = "development"
+	}
+
+	// define empty error
+	// load whatever environment we are in
+	if environmentString == "development" {
+		gin.SetMode(gin.DebugMode)
+
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+
+	}
+	// load the environment file
+	err := godotenv.Load(fmt.Sprintf("%s.env", environmentString))
+
+	// check for errors
+	if err != nil {
+		panic(err)
+	}
 	// Create a new Gin router
 	router := gin.Default()
 
