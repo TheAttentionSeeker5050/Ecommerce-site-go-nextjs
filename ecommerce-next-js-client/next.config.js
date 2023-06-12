96337553461c980301573e8cebc2afcd5e249635
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-06-12 12:32:38 -0300

    enabled docker compose nextjs

---
+++ ecommerce-next-js-client/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // server: {
+    //   port: 3001,
+    // },
+    // other configuration options...
+  };
+  
+module.exports = nextConfig;
