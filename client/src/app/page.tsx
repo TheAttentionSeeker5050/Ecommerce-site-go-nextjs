93329f2cce029b8d5b9188ca732564a5fcba0b25
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-06-17 03:02:13 -0300

    created a toggle switch reusable component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 import { ButtonRedirectPrimary, ButtonWithActionPrimary} from "@/components/buttons/buttonPrimary";
 import { ButtonRedirectSecondary, ButtonWithActionSecondary } from "@/components/buttons/buttonSecondary";
+import { ToggleSwitch } from "@/components/buttons/toggleSwitch";
 
 
 export default function Page() {
     return <div>
         <h1>Hello, Next.js!</h1>
+        {/* <ToggleSwitch label="Toggle Switch" onChange={(value) => console.log(value)} /> */}
+        <ToggleSwitch  onChange={(value) => console.log("toggled value on sw: "+value)}/>
     </div>
 }
