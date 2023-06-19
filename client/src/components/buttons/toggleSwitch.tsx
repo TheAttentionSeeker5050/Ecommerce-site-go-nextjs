ee6554e4aa03309e0d5fd7f2c1bf74d8b3099e43
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-06-19 12:02:08 -0300

    fixed for docker build

--- client/src/components/buttons/toggleSwitch.tsx
+++ client/src/components/buttons/toggleSwitch.tsx
@@ -23,7 +23,7 @@ export function ToggleSwitch(props: Props) {
     return (
         <div className="mt-5">
             <label className="relative inline-block w-16 h-9 rounded-full">
-                <input type="checkbox" className="peer opacity-0 w-0 h-0"  onClick={(e) => handleChange(e)}/>
+                <input type="checkbox" className="peer opacity-0 w-0 h-0"  onClick={() => handleChange}/>
                 <span className="absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-neutral rounded-full duration-300 before:content-[''] before:absolute before:w-7 before:h-7 before:bottom-1 before:left-1 before:rounded-full
                     before:bg-white before:duration-300 peer-checked:before:translate-x-7 peer-checked:bg-primary"></span>
             </label>
