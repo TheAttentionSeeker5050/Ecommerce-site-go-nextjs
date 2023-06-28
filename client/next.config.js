63deb8a5f16d4a850f7f32a08fdde794dc51883c
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-06-28 12:13:00 -0300

    before enabling cors on golang

--- client/next.config.js
+++ client/next.config.js
@@ -11,20 +11,3 @@ const nextConfig = {
 };
   
 module.exports = nextConfig;
-
-// const withPlugins = require('next-compose-plugins');
-// const withTM = require('next-transpile-modules')(['@tailwindcss/typography']);
-
-// module.exports = withPlugins([withTM], {
-//   reactStrictMode: true,
-//   future: {
-//     webpack5: true,
-//   },
-//   webpack: (config, { isServer }) => {
-//     if (isServer) {
-//       require('./scripts/generate-sitemap');
-//     }
-
-//     return config;
-//   },
-// });
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,47 +1,99 @@
 'use client';
-import { handleRegister } from "@/api/handlers/handleRegister"
+import axios from "axios";
 
-import dotenv from 'dotenv';
-dotenv.config();
+
+import React, {useState} from "react";
 
 
 export default function RegisterPage() {
+    // delcare state variables
+    const [username, setUsername] = useState<string | null | undefined>(null);
+    const [email, setEmail] = useState<string | null | undefined>(null);
+    const [first_name, setFirstName] = useState<string | null | undefined>(null);
+    const [last_name, setLastName] = useState<string | null | undefined>(null);
+    const [phone, setPhone] = useState<string | null | undefined>(null);
+    const [middle_name, setMiddleName] = useState<string | null | undefined>(null);
+    const [password, setPassword] = useState<string | null | undefined>(null);
+    const [password2, setPassword2] = useState<string | null | undefined>(null);
+    
+    
     // this will handle the register page form using axios
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
+    const handleSubmit = async (
+        e: React.FormEvent<HTMLFormElement>
+    ): Promise<void> => {
+        e.preventDefault();
+        // handle the register form here
 
-        // get the form data
-        const formData = new FormData(e.currentTarget)
+        // encode the data to raw json
+        const newUserData = {
+            username: username,
+            email: email,
+            first_name: first_name,
+            last_name: last_name,
+            phone: phone,
+            middle_name: middle_name,
+            password: password,
+            password2: password2,
+        };
 
-        // create a new user object
-        const newUser = {
-            username: formData.get('username'),
-            email: formData.get('email'),
-            first_name: formData.get('first_name'),
-            last_name: formData.get('last_name'),
-            phone: formData.get('phone'),
-            middle_name: formData.get('middle_name'),
-            password: formData.get('password'),
-            password2: formData.get('password2'),
-        }
+        // console.log('NEW USER DATA:', newUserData);
 
-        // print the type of newUser
-        console.log('type of user form data:',typeof newUser)
-        console.log('user from form:', newUser)
+        // prepare the url to send the post register request to
+        const requestURL = process.env.API_URL +'/' + process.env.API_USER_SUBDIR +'/register';
+        // const requestURL = String(process.env.API_URL);
+        console.log('URL:', requestURL);
+        console.log('type of url:', typeof requestURL);
 
-        // const apiUrl = process.env.API_URL;
-        // console.log('env variable', apiUrl)
-        // handle register handler function
-        handleRegister(newUser)
-        // console.log(newUser)
+        
+        // send the post request using axios and the url above
+        axios.post(requestURL, newUserData)
+        .then((res) => {
+            // handle success
+            console.log(res);
+            alert('Registration successful');
+        })
+        .catch((err) => {
+            // handle error
+            console.log('ERROR:', err);
+            alert('Registration failed');
+        })
 
-        // sanitize the data and validate if errors
-        // if errors, display them
-        // if no errors, send the data to the server
-        // if server returns errors, display them
+
 
     }
 
+    // handler for input change
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // change the state of the input
+        switch (e.target.name) {
+            case 'username':
+                setUsername(e.target.value);
+                break;
+            case 'email':
+                setEmail(e.target.value);
+                break;
+            case 'first_name':
+                setFirstName(e.target.value);
+                break;
+            case 'last_name':
+                setLastName(e.target.value);
+                break;
+            case 'phone':
+                setPhone(e.target.value);
+                break;
+            case 'middle_name':
+                setMiddleName(e.target.value);
+                break;
+            case 'password':
+                setPassword(e.target.value);
+                break;
+            case 'password2':
+                setPassword2(e.target.value);
+                break;
+            default:
+                break;
+        };
+    };
 
     return (
         <div id="p-content" className="">
@@ -49,28 +101,28 @@ export default function RegisterPage() {
 
             <form method="post" className=" flex flex-col max-w-xl px-3 mx-auto" onSubmit={(e) => handleSubmit(e)}>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
+                <input type="text" id="username" name="username" onChange={(e) => handleChange(e)} />
 
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" onChange={(e) => handleChange(e)} />
 
                 <label htmlFor="firstName">First Name</label>
-                <input type="text" id="firstName" name="first_name" />
+                <input type="text" id="firstName" name="first_name" onChange={(e) => handleChange(e)} />
 
                 <label htmlFor="lastName">Last Name</label>
-                <input type="text" id="lastName" name="last_name" />
+                <input type="text" id="lastName" name="last_name" onChange={(e) => handleChange(e)} />
 
                 <label htmlFor="phone">Phone</label>
-                <input type="text" id="phone" name="phone" />
+                <input type="text" id="phone" name="phone" onChange={(e) => handleChange(e)} />
 
                 <label htmlFor="middleName">Middle Name</label>
-                <input type="text" id="middleName" name="middle_name" />
+                <input type="text" id="middleName" name="middle_name" onChange={(e) => handleChange(e)} />
 
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
+                <input type="password" id="password" name="password" onChange={(e) => handleChange(e)} />
 
                 <label htmlFor="password2">Confirm Password</label>
-                <input type="password" id="password2" name="password2" />
+                <input type="password" id="password2" name="password2" onChange={(e) => handleChange(e)} />
 
                 <input type="submit" value="Register" className="bg-primary text-white py-2 px-4 m-2 rounded-full" />
             </form>
