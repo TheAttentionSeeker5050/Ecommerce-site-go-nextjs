0f502a0ed9a2d4fa3bb1a418bb6705648a8d9713
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-07-02 19:09:14 -0300

    .

--- server/config/CORSConfig.go
+++ server/config/CORSConfig.go
@@ -5,9 +5,9 @@ import (
 )
 
 func ConfigureCors() cors.Config {
-	CorsConfig := cors.DefaultConfig()
-	CorsConfig.AllowOrigins = []string{"http://127.0.0.1:3000", "http://127.0.0.1:3001", "http://159.65.225.127:3001", "127.0.0.1:3000"} // Replace with your desired allowed origins
 	// CorsConfig.AllowAllOrigins = true
+	CorsConfig := cors.DefaultConfig()
+	CorsConfig.AllowOrigins = []string{"http://127.0.0.1:3000", "http://127.0.0.1:3001", "http://159.65.225.127:3001"} // Replace with your desired allowed origins
 	CorsConfig.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
 	CorsConfig.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Accept", "Access-Control-Allow-Origin", "no-cors"}
 
