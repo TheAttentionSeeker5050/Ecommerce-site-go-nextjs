977f2c28bee0760b80c7744fe5980c83f7172f76
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-07-10 19:50:29 -0300

    separate page disclaimer into a component

---
+++ client/src/components/globalComponents/pageDisclaimerComponent.tsx
@@ -0,0 +1,5 @@
+export default function PageDisclaimerComponent(ToggleDarkMode: any, isDarkMode: any) {
+    return (
+        <p className='text-lg font-bold text-danger text-center dark:text-orange-400'>This is not a real shop. It is just for showing my web developer skills only. Please purchase your pet products somewhere else.</p> 
+    )
+}
