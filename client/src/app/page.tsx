9afc57bca1ff03f5ac35b8e854fe642f42964368
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-07-10 21:08:33 -0300

    created first product main page container

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,28 @@
 'use client'
 
 import { ButtonRedirectPrimary } from "@/components/buttons/buttonPrimary"
-
+import { ButtonRedirectSecondary } from "@/components/buttons/buttonSecondary"
+import ProductHighlightedContainer from "@/components/containers/productHighlightedContainer"
 
 
 export default function Page() {
 
+    // product highlight dummy data
+    const productData = {
+        productName: "Product Name",
+        price: 100,
+        productImageSrc: "https://pixabay.com/get/g24cd529966d1ead6a9aed659f85893f6f4ce048e667aede16e4c0af0a7201b4ca5830dd12ca55c6620b97044a11dbacf_1280.jpg",
+        productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident maxime adipisci nemo cumque sint quia blanditiis minus, inventore rerum necessitatibus nisi fugit doloremque itaque",
+        productHighlight: "taste this delicious product",
+        productLink: "https://www.google.com",
+    }
+
     return (
         <div id="p-content" className="">
+            <ProductHighlightedContainer {...productData} />
+            
+            {/* container for the trending products */}
+            
             {/* <h1 >hello home page</h1>
             <br />
             <br />
@@ -15,20 +30,7 @@ export default function Page() {
             <ButtonRedirectPrimary text="Login" href="/login" />
             <ButtonRedirectPrimary text="Register" href="/register" /> */}
 
-            {/* create a product highlight container with image */}
-            <div className="flex flex-row flex-nowrap w-full justify-around">
-                <div className="">
-                    <img className="w-60" src="https://pixabay.com/get/g24cd529966d1ead6a9aed659f85893f6f4ce048e667aede16e4c0af0a7201b4ca5830dd12ca55c6620b97044a11dbacf_1280.jpg" />
-                    <p className="text-xl">Product Name</p>
-                    <p className="text-xl">$100</p>
-                </div>
-
-                <div className="">
-                    <h1 className="text-2xl font-bold">Product Highlight</h1>
-                    <p className="text-lg">Check out our latest product</p>
-                </div>
-                
-            </div>
+            
 
     
         </div>
