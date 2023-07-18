61440e756860aadc60cbd68feb831cee6f5febc0
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-07-18 16:01:53 -0300

    added unstyled product page

--- client/src/app/products/product/[product_id]/page.tsx
+++ client/src/app/products/product/[product_id]/page.tsx
@@ -1,9 +1,21 @@
+"use client"
 import IndividualProductPageContainer from "@/components/containers/selected-product/selectedProductContainer";
+import { reduxStore } from "@/data/redux/reduxStore";
 
 export default function IndividualProductPage({params}: { params: {product_id: string} }) {
+
+    function getProductData()  {
+        // this function will fetch the product data from the server
+        // and return it
+        // but for the moment it will only return the redux store data
+        return reduxStore.getState().selectedProduct.value;
+    }
+
+    const productData = getProductData();
+
     return ( 
         <div id="p-content" className="w-full">
-            <IndividualProductPageContainer product_id={params.product_id} />
+            <IndividualProductPageContainer product_data={productData} />
         </div>
     )
 }
--- client/src/components/containers/selected-product/selectedProductContainer.tsx
+++ client/src/components/containers/selected-product/selectedProductContainer.tsx
@@ -1,12 +1,47 @@
+import { ButtonWithActionPrimary } from "@/components/buttons/buttonPrimary";
+import formatCurrency from "@/utils/formatCurrency";
 
 
 
-export default function IndividualProductPage({product_id}: { product_id: string }) {
+export default function IndividualProductPage(
+    {product_data}: { product_data: any }
+    ) {
     return ( 
         <div id="p-content" className="w-full">
-            <h1>
-                Hello, Next.js! product page {product_id} page
+            <h1 id="product-title">
+                {product_data.productName}
             </h1>
+            <div id="product-price">
+                {formatCurrency(product_data.price)}
+            </div>
+            <div id="product-image">
+                <img src={product_data.productImageSrc} alt={product_data.productName} className="w-60"/>
+            </div>
+            <div id="productDescription">
+                <span>Product Description:</span> <br/>
+                {product_data.productDescription}
+            </div>
+            {/* and now a purchase widget */}
+            <div id="product-purchase-widget">
+                <span>Quantity</span>
+                {/* a dropdown widget here */}
+                <select name="product-quantity" id="product-quantity-select">
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+                <ButtonWithActionPrimary onClick={
+                    () => {
+                        alert('this button will add the product to the cart')
+                } } text="Add to cart" />
+            </div>
         </div>
     )
 }
