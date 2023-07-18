c855bb1a4a8e223c85b7131c045af46f22d72556
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-07-18 20:14:48 -0300

    added responsivity to product screen

--- client/src/components/containers/selected-product/selectedProductContainer.tsx
+++ client/src/components/containers/selected-product/selectedProductContainer.tsx
@@ -8,19 +8,19 @@ export default function IndividualProductPage(
     ) {
     return ( 
         <div id="p-content" className="max-w-4xl my-8 flex flex-col gap-5 mx-auto">
-            <div id="upper-prod-container" className="grid-rows-5 grid grid-cols-3">
+            <div id="upper-prod-container" className="tablet:grid-rows-4 tablet:grid tablet:grid-cols-3 px-4 flex flex-col gap-3">
 
-                <h2 id="product-title" className="text-lg font-bold col-start-2 col-span-2 row-start-1 row-end-2">
+                <h2 id="product-title" className="text-lg font-bold tablet:col-start-2 tablet:col-span-2 tablet:row-start-1 tablet:row-end-2">
                     {product_data.productName}
                 </h2>
-                <div id="product-price-highlights" className="text-md font-semibold text-red-600 col-start-2 col-end-3 col-span-1 row-start-2 row-end-6">
+                <div id="product-image" className="tablet:col-start-1 tablet:col-span-1 tablet:row-start-1 tablet:row-end-4 mx-auto p-2">
+                    {/* will replace image with a carousell widget element */}
+                    <img src={product_data.productImageSrc} alt={product_data.productName} className=""/>
+                </div>
+                <div id="product-price-highlights" className="text-md font-semibold text-red-600 tablet:col-start-2 tablet:col-end-3  tablet:row-start-2 tablet:row-end-5 p-2">
                     {formatCurrency(product_data.price)}
                 </div>
-                <div id="product-image" className="col-start-1 col-span-1 row-start-1 row-end-6">
-                    {/* will replace image with a carousell widget element */}
-                    <img src={product_data.productImageSrc} alt={product_data.productName} className="w-60"/>
-                </div>
-                <div id="product-purchase-widget" className="p-4 border-2 flex flex-col gap-3 rounded-lg dark:bg-gray-light text-black  col-start-3 col-end-4 row-start-2 row-end-6 ">
+                <div id="product-purchase-widget" className="p-4 border-2 flex flex-col gap-3 rounded-lg dark:bg-gray-light text-black  tablet:col-start-3 tablet:col-end-4 tablet:row-start-2 tablet:row-end-4">
                     <span className="">Quantity</span>
                     {/* a dropdown widget here */}
                     <select name="product-quantity" id="product-quantity-select" className="p-2 border-2 dark:text-black">
@@ -41,7 +41,7 @@ export default function IndividualProductPage(
                     } } text="Add to cart" />
             </div>
             </div>
-            <div id="productDescription" className="">
+            <div id="productDescription" className="px-4">
                 <h2 className="font-semibold text-md my-3">Product Description:</h2> 
                 <div className="text-justify text-sm">
                     {product_data.productDescription.map((paragraph: string, i: number) => {
