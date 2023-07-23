332ef1d3b24e527f7a8ad7976534726ded60eec2
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-07-22 22:41:03 -0300

    made pet categories with reusable component

--- client/src/components/containers/products-browse/categoriesBrowseContainer.tsx
+++ client/src/components/containers/products-browse/categoriesBrowseContainer.tsx
@@ -5,7 +5,8 @@ import Link from "next/link";
 import { useEffect } from "react"
 
 export default function CategoriesBrowseContainer(
-    { productCategories, animalCategories }: { productCategories: any, animalCategories: any }
+    // product categories should be list of objects
+    { productCategories,  title} : { productCategories: any[], title: string }
 ) {
     
     
@@ -14,12 +15,15 @@ export default function CategoriesBrowseContainer(
     // populate the categories array
     return (
         <div id="p-content" className="w-full">
-            <h2 className="text-2xl font-bold text-center">Browse our Products By Category</h2>
+            <h2 className="text-2xl font-bold text-center">
+                {/* Browse our Products By Category */}
+                {title}
+            </h2>
             <div id="product-categories-container" className="grid grid-cols-1 phone:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4 max-w-6xl mx-auto gap-3 p-4 ">
                 
                 {productCategories.map((categoryData) => {
                     return (
-                        <Link href={`/products/category/${categoryData.url_formatted_name}`} key={categoryData.id}>
+                        <Link href={`/products/category/${categoryData.url_formatted_name}`} key={categoryData.id}> 
                             <div className="max-w-xs w-9/12 h-52 mx-auto phone:w-44 phone:h-44 text-white bg-blue-500 grid place-content-center rounded-xl" style={
                                 {
                                     backgroundImage: `url(${categoryData.image_thumbnail_url})`,
@@ -29,8 +33,10 @@ export default function CategoriesBrowseContainer(
                                 }
                             } key={categoryData.id}>
                                 <p className="text-md text-center m-3 p-1 bg-white text-black rounded-md opacity-80">
-                                    {
-                                        categoryData.category_name
+                                    { 
+                                        categoryData.category_name ? categoryData.category_name :
+                                        categoryData.pet_name // if the category name is undefined, then it is a pet type
+                                        // this makes it usable for both product categories and pet types
                                     }
                                 </p>
                             </div>
@@ -39,31 +45,7 @@ export default function CategoriesBrowseContainer(
                 )}
             </div>
 
-            <h2 className="text-2xl font-bold text-center">Browse our Products By Pet</h2>
-            <div id="product-categories-container" className="grid grid-cols-1 phone:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4 max-w-6xl mx-auto gap-4 p-4 ">
-                {animalCategories.map((categoryData) => {
-                    return (
-                        <Link href={`/products/category/${categoryData.url_formatted_name}`} key={categoryData.id}>
-                            <div className="max-w-xs w-9/12 h-52 mx-auto phone:w-44 phone:h-44 text-white bg-blue-500 grid place-content-center rounded-xl" style={
-                                {
-                                    backgroundImage: `url(${categoryData.image_thumbnail_url})`,
-
-                                    backgroundSize: "cover",
-                                    backgroundPosition: "center",
-                                    backgroundRepeat: "no-repeat",
-                                }
-                            } key={categoryData.id}>
-                                <p className="text-sm text-center m-3 p-1 bg-white text-black rounded-md opacity-80">
-                                    {
-                                        categoryData.pet_name
-                                    }
-                                </p>
-                            </div>
-                        </Link>
-                    )}
-                )}
-
-            </div>
+            
             
         </div>
     )
