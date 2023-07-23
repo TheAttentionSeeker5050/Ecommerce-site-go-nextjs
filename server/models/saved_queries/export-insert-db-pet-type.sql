efe4b944ad57f9b45f80e179c8a8e01490709af7
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-07-23 01:24:08 -0300

    .

---
+++ server/models/saved_queries/export-insert-db-pet-type.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: select * from ecommerce_db.product_pet_types
+LIMIT 0, 1000
+
+-- Date: 2023-07-23 01:14
+*/
+-- INSERT INTO ecommerce_db.product_pet_types (`id`,`created_at`,`updated_at`,`deleted_at`,`pet_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (1,NULL,NULL,NULL,'Dogs','dogs','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/27fb63ea-dc82-4014-c384-653d165a3e00/public');
+-- INSERT INTO ecommerce_db.product_pet_types (`id`,`created_at`,`updated_at`,`deleted_at`,`pet_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (2,NULL,NULL,NULL,'Cats','cats','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/33ed6a85-d720-4d60-1b25-6df90879a600/public');
+-- INSERT INTO ecommerce_db.product_pet_types (`id`,`created_at`,`updated_at`,`deleted_at`,`pet_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (3,NULL,NULL,NULL,'Birds','birds','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/17f01819-82d8-4cd6-b975-afb890a96f00/public');
+-- INSERT INTO ecommerce_db.product_pet_types (`id`,`created_at`,`updated_at`,`deleted_at`,`pet_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (4,NULL,NULL,NULL,'Small Mammals','small-mammals','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/880eb448-b6a1-4f5b-1805-4ff2edf4fd00/public');
+-- INSERT INTO ecommerce_db.product_pet_types (`id`,`created_at`,`updated_at`,`deleted_at`,`pet_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (5,NULL,NULL,NULL,'Reptiles','reptiles','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/e759e179-b0db-44f2-397c-321482fe9700/public');
+
+select * from ecommerce_db.product_pet_types;
---
+++ server/models/saved_queries/export-insert-db-prod-categories.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: select * from ecommerce_db.product_categories
+LIMIT 0, 1000
+
+-- Date: 2023-07-23 01:12
+*/
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (1,NULL,NULL,NULL,'Food','food','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/cd668e7b-8adb-41a1-28f9-e6eb1ca73a00/public');
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (2,NULL,NULL,NULL,'Treats','treats','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/7eaa0f81-6292-48eb-69f6-7e186a8c6e00/public');
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (3,NULL,NULL,NULL,'Beds and Furniture','beds-and-furniture','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/7fad03a6-6ba9-47f1-1867-811dabce8d00/public');
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (4,NULL,NULL,NULL,'Grooming and Fur Care','grooming-and-fur-care','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/aa670001-35d9-4784-d17c-d5ba293bad00/public');
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (5,NULL,NULL,NULL,'Medicines','medicines','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/9267bb07-18c9-4e1a-0985-79ffdaface00/public');
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (6,NULL,NULL,NULL,'Toys','toys','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/bdbff749-bf17-4099-bcb0-0d3861210d00/public');
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (7,NULL,NULL,NULL,'Collars and Leashes','collars-and-leashes','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/eda9b48f-8650-4909-03c5-f8237d762600/public');
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (8,NULL,NULL,NULL,'Litter','litter','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/56445b34-a134-4836-6810-ef42bca73b00/public');
+-- INSERT INTO ecommerce_db.product_categories (`id`,`created_at`,`updated_at`,`deleted_at`,`category_name`,`url_formatted_name`,`image_thumbnail_url`) VALUES (9,NULL,NULL,NULL,'Habitat Supplies','habitat-supplies','https://imagedelivery.net/TCjTQ2jntwoaDOUnNfWb7Q/c929e9b1-5e2b-492e-468c-3dabb3b18b00/public');
+commit;
+select * from ecommerce_db.product_categories;
