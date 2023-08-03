16321f981c627fbef3956dc2ee50632ecf726feb
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-08-03 01:40:25 -0300

    .

--- client/next.config.js
+++ client/next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     GITHUB_CLIENT_ID_DEV: '31ca4b2ec044339b9570',
     GITHUB_CLIENT_ID_PROD: '05e0f5394297b8e961ae',
     GOOGLE_CLIENT_ID_DEV: '202760226975-e484glaq8qr5q7ks7g6uh74intm8b24u.apps.googleusercontent.com',
-    GOOGLE_CLIENT_ID_PROD: 'not-yet-implemented',
+    GOOGLE_CLIENT_ID_PROD: '202760226975-hlvbv446kgva92jp6gvgiduq0mds7fas.apps.googleusercontent.com',
     // MAINTENANCE_MODE: 'true',
   },
   images: {
