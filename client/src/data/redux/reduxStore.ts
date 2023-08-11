ad96cb0c66b150fd37c65f2d0f370fda077c804a
nicolas <nicolas.castellano.6@hotmail.com>
2023-08-11 11:27:00 -0300

    implemented narrowed down redux persist

--- client/src/data/redux/reduxStore.ts
+++ client/src/data/redux/reduxStore.ts
@@ -1,16 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import productFilterSlice  from "./productFilterStore";
 import sessionIsOpenSlice from "./sessionIsOpenStore";
 
 
-// the store where all the reducers are combined
-export const reduxStore = configureStore({
-    reducer: {
+const persistConfig = {
+    key: "reduxPersistStore",
+    storage,
+    whitelist: ["sessionIsOpen"],
+};
+
+const persistedReducer = persistReducer(persistConfig, 
+    combineReducers({
         // add reducers here
         productFilter: productFilterSlice,
         sessionIsOpen: sessionIsOpenSlice,
-    },
+    })
+);
+
+
+// the store where all the reducers are combined
+export const reduxStore = configureStore({
+    reducer: persistedReducer
 });
+
+export const persistor = persistStore(reduxStore);
+
+
+
 
 export type RootState = ReturnType<typeof reduxStore.getState>;
 export type AppDispatch = typeof reduxStore.dispatch;
