a7ce5e34204beaa02bf8623146fb96cadb18d188
nicolas <nicolas.castellano.6@hotmail.com>
2023-08-14 18:56:41 -0300

    shopping cart functionality done

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ButtonWithActionPrimary } from "@/components/buttons/buttonPrimary";
 import { fetchShoppingCart } from "@/data/redux/api/cartAPIHandlers";
 import { reduxStore } from "@/data/redux/reduxStore";
 import { setShoppingCart } from "@/data/redux/shoppingCartStore";
@@ -65,10 +66,41 @@ export default function ShoppingCartPage() {
         }
     }
 
+    const handleGetCartItemQuantity = async (
+        event: React.ChangeEvent<HTMLInputElement>,
+        productId: string,
+    ) => {
+        // edit the quantity of the item in the cart local state
+        const newCart = cart.map((item:any) => {
+            // attempt to parse the value as an integer number
+            const intQuantity = parseInt(event.target.value);
+
+            // if the value is not a number, return the item
+            if (isNaN(intQuantity)) {
+                return item;
+            }else {
+
+                if (item.productId == productId) {
+                    return {
+                        ...item,
+                        quantity: intQuantity
+                    };
+                } else {
+                    return item;
+                } 
+            }
+            });
+            
+        
+
+        // set the cart state
+        setCart(newCart as any);
+        
+    }
 
     return (
         <div id="p-content" className="">
-            <h1 className="text-center text-center  text-2xl font-bold my-5 ">Your Shopping Cart</h1>
+            <h1 className="text-center text-center  text-2xl font-bold my-6 ">Your Shopping Cart</h1>
 
             {isLoading ?
                 <div className="text-center text-lg">Loading...</div>
@@ -76,15 +108,37 @@ export default function ShoppingCartPage() {
                 isError ?
                     <div className="text-center text-lg">Error...</div>
                 :
-                    <div className="w-full">
-                        {cart.map((item:{productId: string, quantity: number}) => {
+                    <div className="max-w-lg flex p-4 gap-4 flex-col mx-auto border-brand-vivid border-t-2">
+                        {cart.map((item:any) => {
                             return (
-                                <div className="flex flex-row gap-2 justify-between">
-                                    {item.productId}
+                                <div className="grid grid-rows-3 gap-2 grid-cols-6 m-2 border-brand-vivid border-b-2"  key={item.productId}>
+                                    <p className="text-md font-bold col-start-1 col-span-4 row-start-1 row-span-1 ">
+                                        {item.productName}
+                                    </p>
+                                    <p className="text-sm col-start-1 col-span-2 row-start-2 row-span-1">
+                                        Item Id: {item.productId}
+                                    </p>
+                                    <p className="text-xl text-brand-vivid text-right col-start-6 col-span-2 row-start-1 row-span-1">
+                                        ${item.productPrice}
+                                    </p>
+                                    <p className="text-md text-right col-start-5 col-span-3 row-start-2 row-span-2 flex flex-row gap-3 justify-end">
+                                        Quantity: 
+                                        {/* add an input box here */}
+                                        <input className="w-16 h-6 border-2 border-black rounded-lg p-3" type="number" value={item.quantity} onChange={(event) => handleGetCartItemQuantity(event, item.productId)}/>
+                                    </p>
                                 </div>
                             )
                         })}
+
+                        <ButtonWithActionPrimary
+                            text="Update Cart"
+                            onClick={() => {
+                                // fetch the cart items
+                                alert("Made Purchase");
+                            }}
+                        />
                     </div>
+
             }
 
         </div>
